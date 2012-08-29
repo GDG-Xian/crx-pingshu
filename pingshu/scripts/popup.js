aca90ff4bbbc28818cad40d77a7b4f20e6fc917b
greatghoul <greatghoul@gmail.com>
2012-08-29 22:55:33 +0800

    增加评书节目列表和评书分集列表的导航切换
    1. 点击导航菜单时，切换专辑列表和分集列表
    2. 点击专辑时，加载专辑分集信息并切换到播放列表

--- pingshu/scripts/popup.js
+++ pingshu/scripts/popup.js
@@ -1,10 +1,38 @@
 var app = chrome.extension.getBackgroundPage();
 var tpl = {
     genre: '<dt id="genre-%{ID}" class="genre">%{NAME}</dt><dd><ul class="list"></ul></dd>',
-    album: '<li id="album-%{ID}" data-id="%{ID}" data-url="%{URL}" class="album"><ahref="#">%{NAME}</a></li>'
+    album: '<li id="album-%{ID}" data-id="%{ID}" data-url="%{URL}" class="album" data-title="%{NAME}" title="${NAME}"><a href="#">%{NAME}</a></li>',
+    track: '<li id="track-%{ID}" data-trace="%{TRACE}" data-url="%{URL}" data-mp3="%{MP3}" class="track"><a href="#">%{TITLE}</a></li>'
 };
 
 $(function() {
+    var $track_list = $('#track-list');
+    var $album_list = $('#album-list');
+    var $navbar = $('#navbar');
+    var $nav_track_list = $('#nav-track-list');
+    var $nav_album_list = $('#nav-album-list');
+
+    function init_nav_tabs() {
+        $('navs a').click(function(evt) {
+            evt.preventDefault();
+
+            var $this = $(this);
+            var current = $navbar.data('current');
+
+            $navbar.find('a[href=' + current + ']').removeClass('current');
+            $(current).hide();
+            var target_selector = $this.attr('href');
+            var $target_container = $(target_selector);
+            $target_container.show();
+            $navbar.data('current', target_selector);
+            $this.addClass('current');
+        });
+    }
+
+    // 初始化导航标签
+    init_nav_tabs();
+    $nav_album_list.trigger('click');
+
     $('.genre').live('click', function() {
         $(this).next('dd').slideToggle('fast'); 
     });
@@ -13,8 +41,10 @@ $(function() {
         var $this = $(this);
         var album = $this.data();
         app.get_tracks(album, function(tracks) {
+            $nav_track_list.html(album.title).show().trigger('click');
+            $track_list.empty();
             $.each(tracks, function(i, track) {
-                console.log(track);
+                $(fmt(tpl.track, track)).appendTo($track_list);
             }); 
         });
     });
