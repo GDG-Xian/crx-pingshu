9da16fc759552b62ac17758ffb5df58d70bba4e9
greatghoul <greatghoul@gmail.com>
2012-09-04 23:35:51 +0800

    收听评书页面使用 html5 audio 替代 windows media player 播放评书

--- pingshu/js/contentscript.js
+++ pingshu/js/contentscript.js
@@ -1,3 +1,48 @@
+PATTERNS = {
+    PLAYER_PAGE: /http\:\/\/.*\.5ips\.net\/car_(\d+)_(\d+)\.htm/ig,
+    DOWN_LINK: /<li>下载方式一（普通下载）：<a href=\"(.*?)\">/ig
+};
+
+TPLS = {
+    AUDIO: '<audio id="pingshu-player" autoplay="autoplay" controls="controls" src=""></audio>'
+};
+
+var $toolbar, $player;
+
+function dispatcher(url) {
+    if (PATTERNS.PLAYER_PAGE.test(url)) {
+        player_page_handler({
+            album_id: RegExp.$1,
+            track_id: RegExp.$2,
+            url: url
+        });
+    }   
+}
+
+function player_page_handler(detail) {
+    var down_url = detail.url.replace('/car_', '/down_');
+    $.get(down_url, function(html) {
+        var match = PATTERNS.DOWN_LINK.exec(html);
+        var mp3_url = match && match[1];
+        console.log(mp3_url);
+        $player.attr('src', encodeURI(mp3_url)); 
+    }).error(function(xhr, textStatus, error) {
+        console.error(error);
+    });
+}
+
+function get_mp3_url(url) {
+    var dl_url = url.replace('/car_', '/down_');
+}
+
 $(function() {
-    $('<div id="crx-toolbar" style="position:fixed;top:0;right:0;background:#050;padding:2px 8px;color:#DDF0B5;">听评书</div>').appendTo($(document.body));
+    // 初始化播放器
+    $player = $(TPLS.AUDIO);
+    $('#czplayer').parent().css('background-image', 'none').empty().append($player);
+    
+    // 添加顶部工具条
+    $toolbar = $('<div id="pingshu-toolbar">听评书 V1.0</div>');
+    $toolbar.appendTo($(document.body));
+
+    dispatcher(location.toString());
 });
